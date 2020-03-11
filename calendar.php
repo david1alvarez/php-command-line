d91c536ca007898e2c13d3368c66b16a429c4d77
david1alvarez <dalvarez@predictiveindex.com>
2020-03-11 16:22:57 -0400

    updates to logic

--- calendar.php
+++ calendar.php
@@ -1,6 +1,83 @@
 <?php
-echo "Today is " . date("Y/m/d") . "<br>";
-echo "Today is " . date("Y.m.d") . "<br>";
-echo "Today is " . date("Y-m-d") . "<br>";
-echo "Today is " . date("l");
+//https://stackoverflow.com/questions/5536041/echo-all-months-with-days-calendar
+//https://www.w3schools.com/php/phptryit.asp?filename=tryphp_func_date
+//https://www.w3schools.com/php/func_date_date.asp
+function getDates($year)
+{
+    $dates = array();
+
+    // determine if the year is a leap year
+    date("L", mktime(0,0,0, 7,7, $year)) ? $days = 366 : $days = 365;
+    for($i = 1; $i <= $days; $i++){
+        $month = date('m', mktime(0,0,0,1,$i,$year));
+        $wk = date('W', mktime(0,0,0,1,$i,$year));
+        $wkDay = date('D', mktime(0,0,0,1,$i,$year));
+        $day = date('d', mktime(0,0,0,1,$i,$year));
+
+        $dates[$month][$wk][$wkDay] = $day;
+    } 
+
+    return $dates;   
+}
+
+function shiftDays($array) {
+    $lastElement = array_pop($array);
+    array_unshift($array, $lastElement);
+    return $array;
+}
+
+function getDaysForMonth($month, $year)
+{
+    $dates = array();
+    $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+
+    for($i = 1; $i <= $daysInMonth; $i++){
+        $month = date('m', mktime(0,0,0,$month,$i,$year));
+        $week = date('w', mktime(0,0,0,$month,$i,$year));
+        $weekDay = date('D', mktime(0,0,0,$month,$i,$year));
+        $day = date('d', mktime(0,0,0,$month,$i,$year));
+
+        $dates[$month][$week][$weekDay] = $day;
+    }
+
+    return $dates;
+}
+
+
+    $weekdays = array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
+    $weekdaysAbbreviated = array('Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa');
+    $dates = getDaysForMonth(3,2020);
+
+    foreach($dates as $month => $weeks) {
+
+        echo implode(' ', $weekdaysAbbreviated);
+        foreach($weeks as $week => $days) {
+            echo "\r\n";
+            $datesInWeek = array();
+            foreach($weekdays as $day) {
+                array_push($datesInWeek, isset($days[$day]) ? $days[$day] : 'xx');
+            }
+            $datesInWeek = shiftDays($datesInWeek);
+            foreach($datesInWeek as $date) {
+                echo $date;
+                echo ' ';
+            }
+        }
+    }
+    // foreach($dates as $month => $weeks) {
+
+    //     echo implode(' ', $weekdaysAbbreviated);
+    //     foreach($weeks as $week => $days) {
+    //         echo "\r\n";
+    //         $datesInWeek = array();
+    //         foreach($weekdays as $day) {
+    //             array_push($datesInWeek, isset($days[$day]) ? $days[$day] : 'xx');
+    //         }
+    //         $datesInWeek = shiftDays($datesInWeek);
+    //         foreach($datesInWeek as $date) {
+    //             echo $date;
+    //             echo ' ';
+    //         }
+    //     }
+    // }
 ?>
--- test.php
+++ test.php
@@ -1,9 +1,12 @@
 <?php
 //https://stackoverflow.com/questions/5536041/echo-all-months-with-days-calendar
+//https://www.w3schools.com/php/phptryit.asp?filename=tryphp_func_date
+//https://www.w3schools.com/php/func_date_date.asp
 function getDates($year)
 {
     $dates = array();
 
+    // determine if the year is a leap year
     date("L", mktime(0,0,0, 7,7, $year)) ? $days = 366 : $days = 365;
     for($i = 1; $i <= $days; $i++){
         $month = date('m', mktime(0,0,0,1,$i,$year));
@@ -17,24 +20,56 @@ function getDates($year)
     return $dates;   
 }
 
+function getDaysForMonth($month, $year)
+{
+    $dates = array();
+    $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+
+    for($i = 1; $i <= $daysInMonth; $i++){
+        $month = date('m', mktime(0,0,0,$month,$i,$year));
+        $week = date('W', mktime(0,0,0,$month,$i,$year));
+        $weekDay = date('D', mktime(0,0,0,$month,$i,$year));
+        $day = date('d', mktime(0,0,0,$month,$i,$year));
+
+        $dates[$month][$week][$weekDay] = $day;
+    }
+
+    return $dates;
+}
+
+
+    $weekdays = array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
+    $dates = getDaysForMonth(3,2020);
+
+    foreach($dates as $month => $weeks) {
+        echo implode(' ', $weekdays);
+        foreach($weeks as $week => $days) {
+            foreach($weekdays as $day) {
+                echo isset($days[$day]) ? $days[$day] : ' ';
+            }
+        }
+    }
 ?>
 
-<?php $dates = getDates(2011); 
+<?php //$dates = getDates(2011); 
 
-$weekdays = array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'); ?>
-<?php foreach($dates as $month => $weeks) { ?>
-<table>
-    <tr>
-        <th><?php echo implode('</th><th>', $weekdays); ?></th>
-    </tr>
-    <?php foreach($weeks as $week => $days){ ?>
-    <tr>
-        <?php foreach($weekdays as $day){ ?>
-        <td>
-            <?php echo isset($days[$day]) ? $days[$day] : '&nbsp'; ?>
-        </td>               
-        <?php } ?>
-    </tr>
-    <?php } ?>
-</table>
-<?php } ?>
+// 
+// $divider = '--------------------';
+// 
+// $weekdays = array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'); ?>
+<!-- <?php foreach($dates as $month => $weeks) { ?> -->
+<!-- <table> -->
+    <!-- <tr> -->
+        <!-- <th><?php echo implode('</th><th>', $weekdays); ?></th> -->
+    <!-- </tr> -->
+    <!-- <?php foreach($weeks as $week => $days){ ?> -->
+    <!-- <tr> -->
+        <!-- <?php foreach($weekdays as $day){ ?> -->
+        <!-- <td> -->
+            <!-- <?php echo isset($days[$day]) ? $days[$day] : '&nbsp'; ?> -->
+        <!-- </td>                -->
+        <!-- <?php } ?> -->
+    <!-- </tr> -->
+    <!-- <?php } ?> -->
+<!-- </table> -->
+<!-- <?php } ?> -->
